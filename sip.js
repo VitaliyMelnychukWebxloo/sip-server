85eebec15fc92aa995bc5dbe3b927fbff9b01e88
Manu Raghavan <manur@gatech.edu>
2015-05-16 02:49:01 +0800

    Undo release handling

--- sip.js
+++ sip.js
@@ -541,14 +541,10 @@ function makeStreamTransport(protocol, connect, createServer, callback) {
 
       return {
         release: function() {
-					console.log("skipping release");
-					/*
           if(onError) stream.removeListener('error', onError);
           if(--refs === 0) stream.emit('no_reference');
-					*/
         },
         send: function(m) {
-					console.log("Stream transport send: " + util.inspect(stream.address()));
           stream.write(stringify(m), 'ascii');
         },
         protocol: protocol
@@ -659,11 +655,8 @@ function makeWsTransport(options, callback, onClose) {
                   send(m);
               },
         release: function() {
-					console.log("Skipping release");
-					/*
           if(onError) socket.removeListener('error', onError);
           if(--refs === 0) socket.terminate();
-					*/
         },
         protocol: 'WS'
       };
