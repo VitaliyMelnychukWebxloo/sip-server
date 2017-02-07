4296901921e90abe5d0a266b4a48b74c1cc8d452
Shane Gibbons <shane.r.gibbons@gmail.com>
2017-02-07 13:54:47 +0800

    (somewhat naive) guard against the switch crashing if via[0].params isn't defined

--- sip.js
+++ sip.js
@@ -1223,7 +1223,7 @@ function createClientTransaction(rq, transport, tu, cleanup) {
 function makeTransactionId(m) {
   if(m.method === 'ACK')
     return ['INVITE', m.headers['call-id'], m.headers.via[0].params.branch].join();
-  return [m.headers.cseq.method, m.headers['call-id'], m.headers.via[0].params.branch].join();
+  return [m.headers.cseq.method, m.headers['call-id'], (m.headers.via[0].params && m.headers.via[0].params.branch)].join();
 }
 
 function makeTransactionLayer(options, transport) {
